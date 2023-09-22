7c0e9a72fc34479d819c886d9ceac3161dae6882
IT Burleigh Grp <it@burleighgrp.com>
2023-09-22 16:09:25 +1000

    start da requisicao

--- src/Tabs/Principal.tsx
+++ src/Tabs/Principal.tsx
@@ -1,24 +1,23 @@
-import { Text, VStack, ITextProps, Button, Image, FormControl, Box, Input, Link, Center } from "native-base";
-import { useState, ReactNode } from 'react';
+import { Text, VStack, ITextProps, Button, Image, FormControl, Box, Input, Link, Center, ScrollView } from "native-base";
+import { useState, ReactNode, useEffect } from 'react';
 import {styles} from '../style';
 import { TouchableOpacity, StyleSheet  } from 'react-native';
 import Logo from '../assets/Flockdlogo.png';
 
 export default function Principal({navigation}) {
     // const navigation = useNavigation()
+    const [inventory, setInventory] = useState([])
+    useEffect(() => {
+        const teste = fetch('localhost:3334/items')
+        .then((response) => {response.json()})
+        .catch((error) => {console.warn('TESTANDO....', error)})
+        console.warn(teste)
+    },[])
     return (
-      <VStack flex={1} alignItems="center" justifyContent="flexStart" p={5}>
-        <Image source={Logo} alt="Logo Flockd" alignContent="" />
-            <Button 
-            w="100%"
-            bg='blue.800'
-            marginTop={5}
-            borderRadius="lg"
-            onPress={() => navigation.navigate('Tabs')}
-            >
-              log in
-            </Button>           
-      </VStack>
+      <ScrollView flex={1} p={5} >
+            <Image source={Logo} alt="Logo Flockd" />
+            <Text>LIST A SER INCLUIDA</Text>          
+      </ScrollView>
     );
   }
   
