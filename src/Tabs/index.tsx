b8628abf28feb7f15dd51822fa3b9fb2f3303d84
IT Burleigh Grp <it@burleighgrp.com>
2023-09-22 12:05:15 +1000

    Tab bar estilizada

--- src/Tabs/index.tsx
+++ src/Tabs/index.tsx
@@ -1,21 +1,41 @@
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Principal from "./Principal";
 import Profile from "./Profile";
+import Ionicons from 'react-native-vector-icons/Ionicons'
+import {styles} from '../style'
 
 const Tab = createBottomTabNavigator()
 
 export default function Tabs (){
     return(
-        <Tab.Navigator>
+        <Tab.Navigator
+            screenOptions={{
+                tabBarStyle: {
+                    backgroundColor: '#f58b6b'
+                },
+                tabBarActiveTintColor: '#fff',
+                tabBarInactiveTintColor: '#eccec0'
+            }}
+        >
             <Tab.Screen
                 name ="Principal"
                 component={Principal} 
-                options={{headerShown: false}}
+                options={{
+                    headerShown: false, 
+                    tabBarIcon: ({color, size}) => (
+                        <Ionicons name="home" color='{color}' size= {size}/>
+                    )
+                }}
             />
             <Tab.Screen
                 name ="Profile"
                 component={Profile} 
-                options={{headerShown: false}}
+                options={{
+                    headerShown: false,
+                    tabBarIcon: () => (
+                        <Ionicons name="person" color='#fff' size= {20} />
+                    )
+                }}
             />
         </Tab.Navigator>
     )
