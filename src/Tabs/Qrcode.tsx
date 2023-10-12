bd7bb93e0186db77f864f2079efe26a994d6fa93
IT Burleigh Grp <it@burleighgrp.com>
2023-10-12 16:18:42 +1000

    changing the color of the button inside QRCODE GENERATOR

--- src/Tabs/Qrcode.tsx
+++ src/Tabs/Qrcode.tsx
@@ -1,9 +1,11 @@
 // import Logo from '../assets/Flockdlogo.png'
-import { Button, StyleSheet, View } from 'react-native'
+import { Button, Image } from 'native-base'
+import { StyleSheet, View } from 'react-native'
 import { IStackScreenprops } from '@/Library/IStackScreenProps'
 import { IQRCodePayload } from '@/Library/IQRCodePayload'
 import QRCode from 'react-native-qrcode-svg'
 import React from 'react'
+import Logo from '../assets/Flockdlogo.png'
 
 // eslint-disable-next-line react/function-component-definition
 const QRCodeScreen: React.FunctionComponent<IStackScreenprops> = (props) => {
@@ -15,12 +17,19 @@ const QRCodeScreen: React.FunctionComponent<IStackScreenprops> = (props) => {
 
   return (
     <View style={styles.container}>
+      <Image source={Logo} alt="Logo Flockd" marginBottom={80} />
       <QRCode value={JSON.stringify(payload)} />
       <View style={styles.button}>
         <Button
-          title="Go to Scanner"
+          w="100%"
+          bg="blue.800"
+          marginTop={5}
+          borderRadius="lg"
+          textAlign="center"
           onPress={() => navigation.navigate('Scan')}
-        />
+        >
+          Go to Scanner
+        </Button>
       </View>
     </View>
   )
